1afb8f8e029d4d1ef436aaa0c0c397896d3c1498
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2024-11-27 14:59:51 +0100

    dice klar

--- micro_store becomes casino vinterprojekt/Program.cs
+++ micro_store becomes casino vinterprojekt/Program.cs
@@ -3,51 +3,76 @@
 Random rnd = new Random();
 int money = 100;
 bool cashout = false;
+bool workingbet = false;
 string game = "";
-Console.WriteLine($"you have {money} money");
+int bet=0;
+string betString;
+
 
 
 while (money > 0 && cashout == false)
 {
+    Console.WriteLine($"you have {money} money");
     Console.WriteLine("what game do you want to play?");
-    Console.WriteLine("rules");
+    Console.WriteLine("write rewards for rewards list");
     while (game != "roulette" && game != "dice" && game != "blackjack")
     {
         Console.WriteLine("roulette, dice or blackjack");
+        game = Console.ReadLine();
+        if (game == "rewards")
+        {
+            Console.WriteLine("dice can win you 3x on a sum of seven and 5x on a sum of 2 or 12");
+            Console.WriteLine("roulette can win you 2x on a right color guess and 50x on a right number guess");
+            Console.WriteLine("blackjack rewards 2x on win");
+        }
+    }
+    while (workingbet == false)
+    {
+        Console.WriteLine("how much do you bet");
+        betString = Console.ReadLine();
+       workingbet=  int.TryParse(betString, out bet);
+    }
+money = money-bet;
+
+    while (game == "dice")
+    {
+        Console.WriteLine("ready?");
+        Console.ReadLine();
+        int dieOne = rnd.Next(1, 6);
+        int dieTwo = rnd.Next(1, 6);
+        int dieSum = dieOne + dieTwo;
+        Console.WriteLine($"rolled a {dieSum}");
+        if (dieSum == 2 || dieSum == 12) {
+            money= money+bet*5;
+            Console.WriteLine($"nice you win {bet*5} money");
+        }
+        if (dieSum == 7) {
+            money= money+bet*2;
+            Console.WriteLine($"you win {bet*2} money");
+        }
+        if (dieSum != 2||dieSum !=12||dieSum !=7) Console.WriteLine("toobad you did not win");
+
+        Console.WriteLine("again? if not type no otherwise type anything else");
+        string dicecontinue = Console.ReadLine();
+        if (dicecontinue == "no") game = "";
+    }
+
+
+    if (game == "roulette")
+    {
+
+
+
+    }
+
+
+    if (game == "blackjack")
+    {
+
+
+
     }
 }
-
-
-if(game=="dice"){
-int bet=0;
-bool realBetDice = false;
-while(realBetDice==false){
-Console.WriteLine("how much do you bet");
-string betInLetters =Console.ReadLine();
-realBetDice = int.TryParse(betInLetters, out bet);
-}
-int dieOne=rnd.Next(1,6);
-int dieTwo=rnd.Next(1,6);
-int dieSum=dieOne+dieTwo;
-if(dieSum==2 || dieSum==12);
-
-
-}
-
-
-if(game=="roulette"){
-
-
-
-}
-
-
-if(game=="blackjack"){
-
-
-
-}
-
 
 
 
